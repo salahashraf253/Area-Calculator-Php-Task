66e21c085e7146ee6a33338515c43511e82a5386
Salah Ashraf <salahashraf924@gmail.com>
2023-07-29 23:40:59 +0300

    Feat: Add Triangle class for calculating triangle area

---
+++ public/Shapes/Traingle.php
@@ -0,0 +1,15 @@
+<?php
+
+class Triangle implements Shape {
+    private $base;
+    private $height;
+
+    public function __construct($base, $height) {
+        $this->base = $base;
+        $this->height = $height;
+    }
+
+    public function calculateArea() {
+        return 0.5 * $this->base * $this->height;
+    }
+}
